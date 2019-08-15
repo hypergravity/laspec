32f5a913aa53cb124c0028d26747dd9ab87e2515
Bo Zhang <bozhang@nao.cas.cn>
2019-08-15 16:38:36 +0800

    version 2019.0815.0

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+rm -rf build
+rm -rf dist
+#python setup.py build_ext --inplace
+#python setup.py install
+python setup.py sdist
+pip install dist/*.tar.gz
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='laspec',
+    version='2019.0815.0',
+    author='Bo Zhang',
+    author_email='bozhang@nao.cas.cn',
+    description='Modules for LAMOST spectra.',  # short description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/hypergravity/laspec',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Astronomy"],
+    package_dir={'laspec': 'laspec'},
+    include_package_data=True,
+    requires=['numpy', 'scipy']
+)
