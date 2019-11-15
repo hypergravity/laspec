8de5b95a399f79877436574eca38b4b2fb9f1bb7
Bo Zhang <bozhang@nao.cas.cn>
2019-11-15 21:21:38 +0800

    added wavelength conversion between air and vac

---
+++ laspec/wavelength.py
@@ -0,0 +1,25 @@
+"""
+This module implements the conversion of wavelengths between vacuum and air
+Reference: Donald Morton (2000, ApJ. Suppl., 130, 403)
+VALD3 link: http://www.astro.uu.se/valdwiki/Air-to-vacuum%20conversion
+"""
+
+import numpy as np
+
+
+def vac2air(wave_vac):
+    wave_vac = np.array(wave_vac)
+    s = 1e4 / wave_vac
+    n = 1 + 0.0000834254 + \
+        0.02406147 / (130 - s**2) + \
+        0.00015998 / (38.9 - s**2)
+    return wave_vac / n
+
+
+def air2vac(wave_air):
+    wave_air = np.array(wave_air)
+    s = 1e4 / wave_air
+    n = 1 + 0.00008336624212083 + \
+        0.02408926869968 / (130.1065924522 - s ** 2) + \
+        0.0001599740894897 / (38.92568793293 - s ** 2)
+    return wave_air * n
