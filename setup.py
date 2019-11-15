f374e6dfaca5bd10a90ebf752d64a7bb0e7c8ef0
Bo Zhang <bozhang@nao.cas.cn>
2019-11-15 22:44:03 +0800

    version 2019.1115.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='laspec',
-    version='2019.1102.0',
+    version='2019.1115.0',
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description='Modules for LAMOST spectra.',  # short description
