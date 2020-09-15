ca4c5375d82222d1fcdf07047dc2644e894fed05
Bo Zhang <bozhang@nao.cas.cn>
2020-09-15 12:42:20 +0800

    updated install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 rm -rf build
 rm -rf dist
 #python setup.py build_ext --inplace
-#python setup.py install
-python setup.py sdist
-pip install dist/*.tar.gz
+python setup.py install
+#python setup.py sdist
+#pip install dist/*.tar.gz
+rm -rf ./*.egg-info
