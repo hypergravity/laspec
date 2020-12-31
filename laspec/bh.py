37e58aa7cb4be44cfa7532bf276e1ee357892ad8
Bo Zhang <bozhang@nao.cas.cn>
2020-12-31 21:09:10 +0800

    added bh module

---
+++ laspec/bh.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sympy
+import astropy.constants as const
+import astropy.units as u
+
+
+def calculate_m2(Pday=14.5, ideg=90, m1=2.16, Kkms=50):
+    Kms = Kkms * 1000.
+    Psec = Pday * 86400.
+    irad = np.deg2rad(ideg)
+
+    m1kg = m1 * u.solMass.to(u.kg)
+    m2kg = sympy.symbols("m2")
+    a = sympy.solve(np.power(m2kg * np.sin(irad), 3) / np.power((m1kg + m2kg), 2) - Psec * np.power(Kms, 3) / (
+                2 * np.pi * const.G.value), m2kg)
+    mass = a[0]
+    return mass / u.solMass.to(u.kg)
+
+
+if __name__ == "__main__":
+    print(calculate_m2())
+
