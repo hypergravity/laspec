f15d1868827b8b9378b1e155dd8cf21e60e10a9b
Bo Zhang <bozhang@nao.cas.cn>
2021-01-23 18:28:13 +0800

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,7 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark",
-              "sphinx.ext.autodoc",
+extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.napoleon",
               "sphinx.ext.viewcode",
               "sphinx.ext.mathjax",
