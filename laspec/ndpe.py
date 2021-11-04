e9d5654fc4d492b237491368313c77a01513f891
Bo Zhang <bozhang@nao.cas.cn>
2021-11-04 13:47:50 +0800

    rm fof.py

---
+++ laspec/ndpe.py
@@ -0,0 +1,22 @@
+"""
+ND Parameter Estimator
+
+This module aims to estimate parameters from a grid of synthetic spectral library.
+
+Case I: The spectral templates are built on a regular grid
+
+Case II: The spectral templates are irregularly distributed
+
+"""
+
+
+class NDPE:
+
+    isregular = True
+
+    def __init__(self, X, y, isregular=True):
+        pass
+
+    def predict(self):
+        pass
+
