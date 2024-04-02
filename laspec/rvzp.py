d3e794bcc4625aea85e32fec64a43dd8f673ecf7
BO ZHANG <bozhang@nao.cas.cn>
2024-04-02 12:10:04 +0800

    update rvzp code

--- laspec/rvzp.py
+++ laspec/rvzp.py
@@ -1,12 +1,32 @@
+"""
+A simplified version of RVZP calculation algorithm (Zhang et al. 2021).
+ADS Link: https://ui.adsabs.harvard.edu/abs/2021ApJS..256...14Z/abstract
+"""
+
+__all__ = ["calibrate_rvzp"]
+
 import numpy as np
+import numpy.typing as npt
 from astropy import table
-from laspec.snstat import eval_xi_1
 from scipy.optimize import minimize
 
+from .snstat import eval_xi_1  # to scale small number statistics
 
-def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B", "R", "Rm"], ncommon_min=5,
-                   rvobs_err_min=1., verbose=True, debug=False):
-    """ A simplified version of Zhang et al. (2021)
+
+def calibrate_rvzp(
+    rvobs: npt.NDArray,
+    rvobs_err: npt.NDArray,
+    rvref: npt.NDArray,
+    rvref_err: npt.NDArray,
+    spid: npt.NDArray,
+    lmjm: npt.NDArray,
+    rvlabels=["B", "R", "Rm"],
+    ncommon_min: int = 5,
+    rv_internal_error: float = 1.0,
+    verbose: bool = True,
+    debug: bool = False,
+) -> tuple[table.Table, npt.NDArray]:
+    """A simplified version of RVZP calculation algorithm (Zhang et al. 2021).
 
     Parameters
     ----------
@@ -27,7 +47,7 @@ def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B"
         labels of RVs
     ncommon_min:
         if common stars more than ncommon_min, evaluate RVZPs
-    rvobs_err_min:
+    rv_internal_error:
         a noise floor due to wavelength calibration and instrumentation defects. defaults to 1. in unit of km/s
     verbose:
         if True, print details
@@ -36,19 +56,32 @@ def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B"
 
     Returns
     -------
-    tseu, ind_map
+    tuple:
+        tseu, ind_map
 
     Notes
     -----
     make sure that the input arrays are not masked
 
+    References
+    ----------
+    https://ui.adsabs.harvard.edu/abs/2021ApJS..256...14Z/abstract
+
     Examples
     --------
     >>> tseu, ind_map = calibrate_rvzp(
-    >>>     np.array(t["rv1_B", "rv1_R"].to_pandas()), np.array(t["rv1_err_B", "rv1_err_R", ].to_pandas()),
-    >>>     np.where(m9["dr2_radial_velocity_gedr3"].mask, np.nan,m9["dr2_radial_velocity_gedr3"].data),
-    >>>     np.where(m9["dr2_radial_velocity_error_gedr3"].mask, np.nan,m9["dr2_radial_velocity_error_gedr3"].data),
-    >>>     m9["spid"], m9["lmjm"], rvlabels=["B", "R"], ncommon_min=5, rvobs_err_min=1., verbose=True, debug=False)
+    >>>     np.array(t["rv1_B", "rv1_R"].to_pandas()),
+    >>>     np.array(t["rv1_err_B", "rv1_err_R", ].to_pandas()),
+    >>>     np.where(m9["dr2_radial_velocity_gedr3"].mask, np.nan, m9["dr2_radial_velocity_gedr3"].data),
+    >>>     np.where(m9["dr2_radial_velocity_error_gedr3"].mask, np.nan, m9["dr2_radial_velocity_error_gedr3"].data),
+    >>>     m9["spid"],
+    >>>     m9["lmjm"],
+    >>>     rvlabels=["B", "R"],
+    >>>     ncommon_min=5,
+    >>>     rv_internal_error=1.,
+    >>>     verbose=True,
+    >>>     debug=False
+    >>> )
 
     """
     if rvobs.ndim == 2:
@@ -61,10 +94,16 @@ def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B"
     # extract unique SEUs
     spid_lmjm = np.array([spid, lmjm]).T
     u_spid_lmjm, c_spid_lmjm = np.unique(spid_lmjm, return_counts=True, axis=0)
-    print("@RVZP: N_SEU = {}, NSTAR_MED = {}".format(len(u_spid_lmjm), np.median(c_spid_lmjm)))
+    print(
+        "@RVZP: N_SEU = {}, NSTAR_MED = {}".format(
+            len(u_spid_lmjm), np.median(c_spid_lmjm)
+        )
+    )
     # construct SEU table
-    tseu = table.Table(data=[u_spid_lmjm, c_spid_lmjm, u_spid_lmjm[:, 0], u_spid_lmjm[:, 1]],
-                       names=["u_spid_lmjm", "c_spid_lmjm", "u_spid", "u_lmjm"])
+    tseu = table.Table(
+        data=[u_spid_lmjm, c_spid_lmjm, u_spid_lmjm[:, 0], u_spid_lmjm[:, 1]],
+        names=["u_spid_lmjm", "c_spid_lmjm", "u_spid", "u_lmjm"],
+    )
     if debug:
         return tseu
 
@@ -93,21 +132,39 @@ def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B"
             this_rvobs = rvobs[this_ind, irv]
             this_rvobs_err = rvobs_err[this_ind, irv]
 
-            this_nobs_fnt = np.sum(np.isfinite(this_rvobs) & np.isfinite(this_rvobs_err))
-            ind_common = np.isfinite(this_rvref) & np.isfinite(this_rvobs) & np.isfinite(this_rvobs_err)
+            this_nobs_fnt = np.sum(
+                np.isfinite(this_rvobs) & np.isfinite(this_rvobs_err)
+            )
+            ind_common = (
+                np.isfinite(this_rvref)
+                & np.isfinite(this_rvobs)
+                & np.isfinite(this_rvobs_err)
+            )
             this_ncommon = np.sum(ind_common)
             this_rvzp["nobs_{}".format(rvlabels[irv])] = this_nobs_fnt
             this_rvzp["ncommon_{}".format(rvlabels[irv])] = this_ncommon
             if this_ncommon >= ncommon_min:
                 # do the rvzp calculation
-                res = minimize(rvzp0cost, x0=np.nanmedian(this_rvref - this_rvobs), method="Nelder-Mead",
-                               args=(this_rvref[ind_common], this_rvref_err[ind_common], this_rvobs[ind_common],
-                                     this_rvobs_err[ind_common], rvobs_err_min))
+                res = minimize(
+                    rvzp0cost,
+                    x0=np.nanmedian(this_rvref - this_rvobs),
+                    method="Nelder-Mead",
+                    args=(
+                        this_rvref[ind_common],
+                        this_rvref_err[ind_common],
+                        this_rvobs[ind_common],
+                        this_rvobs_err[ind_common],
+                        rv_internal_error,
+                    ),
+                )
 
                 this_rvzp["rvzp_{}".format(rvlabels[irv])] = res["x"][0]
-                this_rvzp["rvzp_err_{}".format(rvlabels[irv])] = \
-                    np.ptp(np.nanpercentile(this_rvobs[ind_common] + res["x"][0] - this_rvref[ind_common], [16, 84])) \
-                    / (2 * eval_xi_1(this_ncommon) * np.sqrt(this_ncommon))
+                this_rvzp["rvzp_err_{}".format(rvlabels[irv])] = np.ptp(
+                    np.nanpercentile(
+                        this_rvobs[ind_common] + res["x"][0] - this_rvref[ind_common],
+                        [16, 84],
+                    )
+                ) / (2 * eval_xi_1(this_ncommon) * np.sqrt(this_ncommon))
         if verbose:
             print(this_rvzp, end="\n")
         else:
@@ -126,7 +183,16 @@ def calibrate_rvzp(rvobs, rvobs_err, rvref, rvref_err, spid, lmjm, rvlabels=["B"
     return tseu, ind_map
 
 
-def rvzp0cost(rvzp, this_rvref, this_rvref_err, this_rvobs, this_rvobs_err, rvobs_err_min):
-    return np.nansum(np.abs(this_rvobs + rvzp - this_rvref) / np.sqrt(
-        rvobs_err_min ** 2 + this_rvobs_err ** 2 + this_rvref_err ** 2))
-
+def rvzp0cost(
+    rvzp: npt.NDArray,
+    this_rvref: npt.NDArray,
+    this_rvref_err: npt.NDArray,
+    this_rvobs: npt.NDArray,
+    this_rvobs_err: npt.NDArray,
+    rv_internal_error: float,
+) -> float:
+    """Cost function for RVZP calculation."""
+    return np.nansum(
+        np.abs(this_rvobs + rvzp - this_rvref)
+        / np.sqrt(rv_internal_error**2 + this_rvobs_err**2 + this_rvref_err**2)
+    )
