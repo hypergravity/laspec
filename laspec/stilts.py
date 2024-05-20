208017467327c8d4ec7e9e36d9cfb39736e9003a
BO ZHANG <bozhang@nao.cas.cn>
2024-05-20 21:00:56 +0800

    update stilts wrapper

--- laspec/stilts.py
+++ laspec/stilts.py
@@ -1,4 +1,14 @@
 """
 python wrapper of stilts
-
 """
+
+import os
+from laspec.utils import download
+
+
+def stilts(**kwargs):
+    stilts_executable = download("stilts")
+    cmd = f"java -jar {stilts_executable}"
+    for k, v in kwargs.items():
+        cmd += f" {k} {v}"
+    return os.system(cmd)
