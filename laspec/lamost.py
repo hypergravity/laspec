7b1d053cb2d1a99a2a49a7bcf13217917e219227
BO ZHANG <bozhang@nao.cas.cn>
2025-03-25 21:26:44 +0800

    simplify lamost_filepath()

--- laspec/lamost.py
+++ laspec/lamost.py
@@ -1,211 +1,53 @@
-# -*- coding: utf-8 -*-
 import os
-
+from collections.abc import Iterable
 import numpy as np
 
 
-def lamost_filepath(planid, mjd, spid, fiberid, dirpath="", extname=".fits"):
-    """ generate file path of a LAMOST spectrum
+def lamost_filepath(
+    obsdate: str | Iterable[str],
+    planid: str | Iterable[str],
+    lmjd: int | Iterable[int],
+    spid: int | Iterable[int],
+    fiberid: int | Iterable[int],
+    rootdir="",
+    prefix="spec",
+    extname="fits.gz",
+):
+    """generate file path of a LAMOST spectrum
 
     Parameters
     ----------
-    planid: string
+    obsdate: str
+        obsdate, e.g. "2014-01-01"
+    planid: str
         planid
-
-    mjd: 5-digit integer
-        mjd (use lmjd rather than mjd for DR3 and after!)
-
+    lmjd: int
+        lmjd (use lmjd for DR3 and after!)
     spid: 2-digit integer
         spid, the number of the spectrogragh
-
-    fiberid: 3-digit integer
+    fiberid: int
         fiberid
-
-    dirpath: string
+    rootdir: str
         the root directory for storing spectra.
+    prefix: str
+        prefix of file name. "spec" for LRS and "med" for MRS.
+    extname: str
+        extension name, ".fits.gz" by default.
 
     Returns
     --------
-    filepath: string
+    filepath: str or Iterable
         the path of root dir of directory (prefix).
         if un-specified, return file name.
-
     """
-
-    # pre-processing: strip
-    if np.isscalar(planid):
-        planid = planid.strip()
+    if isinstance(lmjd, str):
+        # scalar mode
+        return f"{rootdir}/{obsdate.replace('-', '')}/{planid.strip()}/{prefix}-{lmjd:05d}-{planid.strip()}_sp{spid:02d}-{fiberid:03d}.{extname}"
     else:
-        planid = [_.strip() for _ in planid]
-
-    if dirpath == "" or dirpath is None:
-        # return file name
-        if np.isscalar(mjd):
-            # if only input one item
-            return "spec-%05d-%s_sp%02d-%03d%s" \
-                   % (mjd, planid, spid, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["spec-%05d-%s_sp%02d-%03d%s" %
-                             (mjd[i], planid[i], spid[i], fiberid[i], extname)
-                             for i in range(len(mjd))])
-    else:
-        # return file path
-        if not dirpath[-1] == os.path.sep:
-            dirpath += os.path.sep
-
-        if np.isscalar(mjd):
-            # if only input one item
-            return "%s%s%sspec-%05d-%s_sp%02d-%03d%s" \
-                   % (dirpath, planid, os.path.sep,
-                      mjd, planid, spid, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["%s%s%sspec-%05d-%s_sp%02d-%03d%s" %
-                             (dirpath, planid[i], os.path.sep, mjd[i],
-                              planid[i], spid[i], fiberid[i], extname)
-                             for i in range(len(mjd))])
-
-
-def lamost_filepath_med(planid, mjd, spid, fiberid, dirpath="",
-                        extname=".fits"):
-    """ generate file path of a LAMOST spectrum (medium resolution)
-
-    Parameters
-    ----------
-    planid: string
-        planid
-
-    mjd: 5-digit integer
-        mjd (use lmjd rather than mjd for DR3 and after!)
-
-    spid: 2-digit integer
-        spid, the number of the spectrogragh
-
-    fiberid: 3-digit integer
-        fiberid
-
-    dirpath: string
-        the root directory for storing spectra.
-
-    Returns
-    --------
-    filepath: string
-        the path of root dir of directory (prefix).
-        if un-specified, return file name.
-
-    """
-
-    # pre-processing: strip
-    if np.isscalar(planid):
-        planid = planid.strip()
-    else:
-        planid = [_.strip() for _ in planid]
-
-    if dirpath == "" or dirpath is None:
-        # return file name
-        if np.isscalar(mjd):
-            # if only input one item
-            return "med-%05d-%s_sp%02d-%03d%s" \
-                   % (mjd, planid, spid, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["med-%05d-%s_sp%02d-%03d%s" %
-                             (mjd[i], planid[i], spid[i], fiberid[i],
-                              extname)
-                             for i in range(len(mjd))])
-    else:
-        # return file path
-        if not dirpath[-1] == os.path.sep:
-            dirpath += os.path.sep
-
-        if np.isscalar(mjd):
-            # if only input one item
-            return "%s%s%smed-%05d-%s_sp%02d-%03d%s" \
-                   % (dirpath, planid, os.path.sep,
-                      mjd, planid, spid, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["%s%s%smed-%05d-%s_sp%02d-%03d%s" %
-                             (dirpath, planid[i], os.path.sep, mjd[i],
-                              planid[i], spid[i], fiberid[i], extname)
-                             for i in range(len(mjd))])
-
-
-def _test_lamost_filepath():
-    """test function **lamost_filepath**
-    """
-    print(lamost_filepath("GAC_061N46_V3", 55939, 7, 78))
-    print(lamost_filepath("GAC_061N46_V3", 55939, 7, 78, "/"))
-    print(lamost_filepath("GAC_061N46_V3", 55939, 7, 78, "/pool"))
-    print(lamost_filepath("GAC_061N46_V3", 55939, 7, 78, "/pool/"))
-
-
-def sdss_filepath(plate, mjd, fiberid, dirpath="", extname=".fits"):
-    """ generate file path of a LAMOST spectrum
-
-    Parameters
-    ----------
-    plate: string
-        plate
-
-    mjd: 5-digit integer
-        mjd (use lmjd rather than mjd for DR3 and after!)
-
-    fiberid: 4-digit integer
-        fiberid
-
-    dirpath: string
-        the root directory for storing spectra.
-
-    extname: string
-        in case that users want to synthesize other data format
-
-    Returns
-    --------
-    filepath: string
-        the path of root dir of directory (prefix).
-        if un-specified, return file name.
-
-    """
-
-    if dirpath == "" or dirpath is None:
-        # return file name
-        if np.isscalar(mjd):
-            # if only input one item
-            return "spec-%04d-%05d-%04d%s" % (plate, mjd, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["spec-%04d-%05d-%04d%s" %
-                             (plate[i], mjd[i], fiberid[i], extname)
-                             for i in range(len(mjd))])
-    else:
-        # return file path
-        if not dirpath[-1] == os.path.sep:
-            dirpath += os.path.sep
-
-        if np.isscalar(mjd):
-            # if only input one item
-            return "%s%04d%sspec-%04d-%05d-%04d%s" \
-                   % (dirpath, plate, os.path.sep,
-                      plate, mjd, fiberid, extname)
-        else:
-            # if input a list of items
-            return np.array(["%s%04d%sspec-%04d-%05d-%04d%s" %
-                             (dirpath, plate[i], os.path.sep, plate[i],
-                              mjd[i], fiberid[i], extname)
-                             for i in range(len(mjd))])
-
-
-def _test_sdss_filepath():
-    print(sdss_filepath(2238, 52059, 1, "/"))
-
-
-if __name__ == "__main__":
-    print("")
-    print("@Cham: start to test the module ...")
-    print("")
-    print("@Cham: testing ""lamost_filepath"" ...")
-    _test_lamost_filepath()
-    _test_sdss_filepath()
-    print("@Cham: OK")
+        # vector mode
+        fps = []
+        for i in range(len(lmjd)):
+            fps.append(
+                f"{rootdir}/{obsdate[i].replace('-', '')}/{planid[i].strip()}/{prefix}-{lmjd[i]:05d}-{planid[i].strip()}_sp{spid[i]:02d}-{fiberid[i]:03d}.{extname}"
+            )
+        return np.array(fps)
