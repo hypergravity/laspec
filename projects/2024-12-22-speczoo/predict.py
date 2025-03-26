311aa2cafc0b6c897e6a9bea15fa006b227b866a
BO ZHANG <bozhang@nao.cas.cn>
2025-03-26 16:01:47 +0800

    update predict.py

--- projects/2024-12-22-speczoo/predict.py
+++ projects/2024-12-22-speczoo/predict.py
@@ -1,1 +1,48 @@
-print("Hello")
+import joblib
+import os
+import sys
+from laspec import MrsSpec
+import numpy as np
+
+WEIGHT_PATH = "/slam/weight.joblib"
+VERBOSE = True
+WAVE = np.arange(3950, 5851, 1.0)
+
+# Read spectrum
+if VERBOSE:
+    print("[1/3] Read Spectrum")
+
+
+def read_spectrum(fp):
+    assert os.path.exists(fp), fp
+    # read FITS spectrum
+    spec = MrsSpec.from_lrs(fp)
+    # correct redshift, normalize and interpolate spectrum
+    flux, flux_err = spec.interp_then_norm(WAVE, rv=None)
+    return flux, flux_err
+
+
+print(sys.argv)
+fp = sys.argv[1]
+print("fp: ", fp)
+flux, flux_err = read_spectrum(fp)
+flux[flux > 2] = 1
+flux[flux < 0] = 1
+
+# Load model (/slam/weight.joblib)
+if VERBOSE:
+    print("[2/3] Load Model")
+assert os.path.exists(WEIGHT_PATH), WEIGHT_PATH
+sp = joblib.load(WEIGHT_PATH)
+
+# Predict labels
+if VERBOSE:
+    print("[3/3] Predict Labels")
+x_pred = sp.least_square(
+    flux,
+    flux_err,
+    p0=[6000, 4, -0.1],
+    method="trf",
+    bounds=(np.ones(3) * -0.6, np.ones(3) * 0.6),
+)
+print(x_pred)
